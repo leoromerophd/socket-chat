82243642141d84f20ce785d92048f8c3a86e1ff0
leoromerophd <leonardosuarezromero@gmail.com>
2020-01-02 20:50:15 -0600

    Base de Aplicacion de Chat

---
+++ public/js/socket-chat.js
@@ -0,0 +1,52 @@
+var socket = io();
+var params = new URLSearchParams(window.location.search);
+
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
+};
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuarios Conectados', resp);
+    })
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+// socket.emit('crearMensaje', {
+//     usuario: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+// Avisar desconección de un usuario 
+socket.on('crearMensaje', function(mensaje) {
+    console.log('Servidor:', mensaje);
+});
+
+// Escuchar cambios de usuarios 
+// cuando un usuario entra o sale del chat 
+socket.on('listaPersona', function(personas) {
+    console.log(personas);
+});
+
+//  Enviar mensajes privados
+
+socket.on('mensajePrivado', function(mensaje) {
+    console.log('Mensaje Privado', mensaje);
+});
